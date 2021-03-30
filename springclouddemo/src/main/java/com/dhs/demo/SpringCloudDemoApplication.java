1d8f20789caa996e8486c5da22ea927865f09822
duhaisen <15764226651@163.com>
2021-03-30 11:03:12 +0800

    rename and add new

--- springclouddemo/src/main/java/com/dhs/demo/SpringCloudDemoApplication.java
+++ springclouddemo/src/main/java/com/dhs/demo/SpringCloudDemoApplication.java
@@ -3,11 +3,14 @@ package com.dhs.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dhs
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class SpringCloudDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringCloudDemoApplication.class, args);
     }
 
 }
--- springdemo/src/main/java/com/dhs/demo/SpringDemoApplication.java
+++ springdemo/src/main/java/com/dhs/demo/SpringDemoApplication.java
@@ -3,11 +3,14 @@ package com.dhs.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dhs
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class SpringDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringDemoApplication.class, args);
     }
 
 }
